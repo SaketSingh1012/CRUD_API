bca676c31d9d2dec25fe2b17316809ec5c0b8176
Saket Singh <saketaryanite@gmail.com>
2023-11-17 10:01:39 +0530

    .env configurations

--- queries.js
+++ queries.js
@@ -1,10 +1,13 @@
 const Pool = require("pg").Pool;
+const dotenv = require('dotenv');
+dotenv.config();
+
 const pool = new Pool({
-  user: "todos",
-  host: "localhost",
-  database: "api",
-  password: "123",
-  port: 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 });
 
 const yup = require("yup");
